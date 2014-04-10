b9989497855da07912d9a2037430ade6a39679da
Dolgikh <dolgikh@Andreys-Mac-mini-2.local>
2014-04-10 16:28:49 +0300

    Initial Commit

---
+++ EmptyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmptyApp
+//
+//  Created by Dolgikh on 4/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EAAppDelegate class]));
+    }
+}
