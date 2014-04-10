1e92b68d9aa794c8cee64be591fca227fcdde88e
Dolgikh <dolgikh@Andreys-Mac-mini-2.local>
2014-04-10 16:33:29 +0300

    ObjC first app

---
+++ EmptyApp/EAViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAViewController.h
+//  EmptyApp
+//
+//  Created by Dolgikh on 4/10/14.
+//  Copyright (c) 2014 Dolgikh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAViewController : UIViewController
+
+@end
---
+++ EmptyApp/EAViewController.m
@@ -0,0 +1,38 @@
+//
+//  EAViewController.m
+//  EmptyApp
+//
+//  Created by Dolgikh on 4/10/14.
+//  Copyright (c) 2014 Dolgikh. All rights reserved.
+//
+
+#import "EAViewController.h"
+
+@interface EAViewController ()
+
+@end
+
+@implementation EAViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
